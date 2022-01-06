3f375d5644987413173fd73c7e5edc85d67fc246
MilttO0nMss <grountish@gmail.com>
2022-01-06 13:58:13 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Switch, Route } from "react-router-dom";
  import Navigation from "./components/Navigation";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Community from "./pages/Community";
-import Profile from "./pages/Profile";
+// import Community from "./pages/Community";
+// import Profile from "./pages/Profile";
 import AdminPage from "./pages/AdminPage";
-import EditProfile from "./pages/EditProfile";
+// import EditProfile from "./pages/EditProfile";
 import Home from "./pages/Home";
 import PublicRoute from "./components/PublicRoute";
 import PrivateRoute from "./components/PrivateRoute";
@@ -24,9 +24,9 @@ function App() {
         <AdminRoute exact path="/admin" component={AdminPage} />
         <PublicRoute exact path="/signup" component={Signup} />
         <PublicRoute exact path="/login" component={Login} />
-        <PrivateRoute exact path="/community" component={Community} />
+        {/* <PrivateRoute exact path="/community" component={Community} />
         <PrivateRoute exact path="/profile" component={Profile} />
-        <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+        <PrivateRoute exact path="/edit-profile" component={EditProfile} /> */}
         <PrivateRoute exact path="/private" component={Private} />
       </Switch>
 
